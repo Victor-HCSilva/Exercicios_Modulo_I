4945e2c03e55e2c7f1305af8db8b71fb80faf1d5
Victor Hugo Clmentino da Silva <victorh.csilva20@gmail.com>
2024-06-21 22:20:00 -0300

    Atualização e melhorias

--- atv_jogo_adivinhe_o_numero.py
+++ atv_jogo_adivinhe_o_numero.py
@@ -5,62 +5,59 @@ Created on Wed Jun 19 22:04:45 2024
 @author: victor
 """
 
-1#jogo
+#Atualizado 21/06
 
 from random import randint
 
 iniciar = True
-
 tentativas = 0
-dificuldade = {'Fácil': 1,'Médio': 2,'Difícil': 3}
+dificuldade = {'Fácil': 1,'Médio': 2,'Difícil': 3} 
 
 while iniciar:
 
     print('\nFácil: 1;\nMédio: 2;\nDifícil: 3.')
-    print('\nEscolha a dificuldade digitando 1,2 ou 3')
+    print('\nEscolha a dificuldade (1, 2 ou 3)')
     escolher_dificuldade = input('')
 
     if escolher_dificuldade.rstrip() == '1':    #Escolhendo a dificuldade: facil, médio ou difcil
         limite = 10
+        dificuldade_escolhida = 'Fácil'
         break
     elif escolher_dificuldade.rstrip() == '2':
         limite = 50
+        dificuldade_escolhida = 'Médio'
         break
     elif escolher_dificuldade.rstrip() == '3':
         limite = 100
+        dificuldade_escolhida = 'Difícil'
         break
     else:
-        print('inválido')
+        print(f'inválido! Você digitou "{escolher_dificuldade}"')
 
-numero_sorteado = randint(1, limite)
+print(f'Muito bém você escolheu a dificuldade "{dificuldade_escolhida}"\n\n')
+
+número_sorteado = randint(1, limite)
 
 while iniciar:
 
   try:
-    numero_do_jogador = int(input(f'Você deve digitar um numero inteiro de 1 a {limite},\nSe o numero for igual ao numero sorteado você ganha!'))
+    número_do_jogador = int(input(f'Você deve digitar um número inteiro de 1 a {limite},\nSe o número for igual ao número sorteado você ganha!'))
 
-    if numero_do_jogador == numero_sorteado:#Se o jogador acertar o numero
+    if número_do_jogador == número_sorteado:#Se o jogador acertar o número
       break
-    elif numero_do_jogador == 1234:#Obtenção da resposta
-      print(f'Resposta: {numero_sorteado}')
+    elif número_do_jogador == 1234:  #Obtenção da resposta caso o jogador desista
+      print(f'Resposta: {número_sorteado}')
       break
-    elif numero_do_jogador > numero_sorteado:#Pequena ajuda para o jogador
-        print(f'\nO numero é menor que {numero_do_jogador}.')
-    elif numero_do_jogador < numero_sorteado:
-        print(f'\nO numero é maior {numero_do_jogador}.')
+    elif número_do_jogador > número_sorteado:  #Pequena ajuda para o jogador
+        print(f'\nO número é menor que {número_do_jogador}.')
+    elif número_do_jogador < número_sorteado:
+        print(f'\nO número é maior {número_do_jogador}.')
     else:
         pass
 
-    denovo = input('\nVocê não acertou! Deseja tentar novamente click (s/n)?')
-
-    if denovo == 'n':#Se errar e não quiser jogar novamente
-        print('\nVocê perdeu.')
-        break
-    else:
-        pass
+    print('\nVocê não acertou :(\n')
 
     tentativas += 1
-
 
   except Exception as erro:
     print(f'Ops, digitou algo inválido: {erro}')
@@ -68,9 +65,9 @@ while iniciar:
     tentativas += 1
 
 if tentativas > 0:
-    print(f'Parabéns você acertou o numero sorteado ({numero_sorteado})! Errou apenas {tentativas} vez(es).')
+    print(f'\n\n\n\nParabéns você acertou o número sorteado ({número_sorteado})! Errou apenas {tentativas} vez(es).')
 else:
-    print('\n\nUal! que sorte acertou de primeira!')
+    print('\n\n\n\nUal! que sorte acertou de primeira!')
 
 
 
