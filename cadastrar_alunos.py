90402774c9923ed6c67785aca4b01b23e416f9fb
Victor Hugo Clmentino da Silva <victorh.csilva20@gmail.com>
2024-06-29 18:21:48 -0300

    adição das alterações
    Agora é possível alterar os dados dentro das opções de alterar e deletar.

--- cadastrar_alunos.py
+++ cadastrar_alunos.py
@@ -249,17 +249,21 @@ Exercício de Programação em Python: Sistema de Gerenciamento de Alunos
 Objetivo: Criar um sistema básico de gerenciamento de alunos em Python, utilizando listas e dicionários para armazenar e manipular os dados dos alunos.
 Requisitos:
 Menu Interativo: Implemente um menu com as seguintes opções:
-Cadastrar Novo Aluno:
+
+------------Cadastrar Novo Aluno:
 Permitir que o usuário digite o nome, curso, matrícula e outras informações relevantes do aluno.
 Armazenar os dados do aluno em um dicionário.
 Adicionar o dicionário do aluno a uma lista que contém todos os alunos cadastrados.
-Listar Alunos Cadastrados:
+
+-------------Listar Alunos Cadastrados:
 Exibir os dados de todos os alunos cadastrados em um formato organizado, incluindo nome, curso e matrícula.
 Sair: Encerrar o programa.
-Validação de Dados:
+
+-------------Validação de Dados:
 Matrícula: Verifique se a matrícula informada é um número inteiro e se não está duplicada na lista de alunos.
 Nome e Curso: Verifique se o nome e o curso não estão vazios.
 Imprima mensagens de erro claras para o usuário em caso de dados inválidos.
+
 Dicas:
 Utilize uma lista para armazenar os dicionários que representam os alunos.
 Use a função input() para receber dados do usuário.
@@ -314,14 +318,15 @@ def salvar(alunos, arquivo = 'registro de aluno fap 2024.csv'):
         writer = csv.writer(arquivo)
 
         #Caso o arquivo já tenha sido criado este código comentado irá ser reescrito .
-        #writer.writerow(('nome', 'matricula', 'curso','presenca(s)','email','informacoes','notas'))
+        #writer.writerow(('nome', 'matricula', 'curso','telefone','email'))
 
         for aluno in alunos:
-            writer.writerow((aluno['nome'],str(aluno['matricula']),aluno['curso'],str(aluno['presencas']),aluno['email'],aluno['informacoes'],str(aluno['notas'])))
+            writer.writerow((aluno['nome'],str(aluno['matricula']),aluno['curso'],aluno['telefone'],aluno['email']))
+
     print('Cadastrado(a).')
 
 def acessar(nome = '', matricula = '0', mudanca = '',tipo = '', arquivo = 'registro de aluno fap 2024.csv', email = ''):
-    with open(arquivo, mode='r') as file:
+    with open(arquivo, mode='r', encoding='utf8') as file:
         reader = csv.reader(file)
         rows = list(reader)
 
@@ -341,48 +346,40 @@ def acessar(nome = '', matricula = '0', mudanca = '',tipo = '', arquivo = 'regis
         print(f'Comparar com: {row[0]}, {row[1]}')
 
 
+
+
         if row[0] == nome and row[1] == matricula and tipo == '1':
             row[0] = mudanca   #---------------------Mudar o nome
             print('\nMudança tipo 1 realizada')
 
-        elif row[0] == nome and row[4] == email and tipo == '2':
+        elif row[0] == nome and tipo == '2':
             row[1] = mudanca #---------------------Mudar o numero de matrícula, precisa do email
             print('Mudança realizada')
 
         elif row[0] == nome and row[1] == matricula and tipo == '3':
-            row[2] = mudanca #---------------------Mudar o CURSO
+            row[2] = mudanca #---------------------Mudar o curso
             print('Mudança realizada')
 
         elif row[0] == nome and row[1] == matricula and tipo == '4':
-            row[3] = mudanca #---------------------Mudar as presencas
+            row[3] = mudanca #---------------------Mudar telefone
             print('Mudança realizada')
 
         elif row[0] == nome and row[1] == matricula and tipo == '5':
             row[4] = mudanca #---------------------Mudar o email
-            print('Mudança realizada')
+            print('\nMudança de email realizada!')
 
         elif row[0] == nome and row[1] == matricula and tipo == '6':
-            row[5] = mudanca #---------------------Mudar informações/observações
-            print('Mudança realizada')
-
-        elif row[0] == nome and row[1] == matricula and tipo == '7':
-            row[6] = mudanca #---------------------Mudar as notas
-            print('Mudança realizada')
-
-        elif row[0] == nome and row[1] == matricula and tipo == '8':
             #---------------------'DELETAR DADOS'-------------------------
 
-            row[0] = replace(row[i],'') #----------trocando o argumanto por 'nada'
+            row[0] = '' #----------trocando o argumanto por 'nada'
             row[1] = ''
             row[2] = ''
             row[3] = ''
             row[4] = ''
-            row[5] = ''
-            row[6] = ''
 
             print('Deleção realizada...')
 
-    # Escrever os dados de volta no arquivo CSV
+    # Escrever os dados de volta no arquivo CSV, reecreve todas as informações
     with open(arquivo, mode='w', encoding='utf8',newline='') as file:
         writer = csv.writer(file)
         writer.writerows(rows)
@@ -408,13 +405,11 @@ def aluno_info(nome = 'teste da silva', matricula = '20230044'):
             print('\nRow', row,'Indicie:',i)
             print(f'\n\nDados gerais de {nome.title()}:')
             print('------------------------------------------')
-            print('Nome do aluno(a):',row[0])
-            print('Matrícula do aluno(a):',row[1])
-            print('Curso do aluno(a):',row[2])
-            print('Presenças do aluno(a) :',row[3])
-            print('Email do aluno(a):',row[4],)
-            print('Informações adicionais do aluno(a) :',row[5])
-            print('Notas do aluno(a):',row[6])
+            print('Nome:',row[0])
+            print('Matrícula:',row[1])
+            print('Curso:',row[2])
+            print('Telefone:',row[3])
+            print('Email:',row[4],)
             print('------------------------------------------', sep = '')
         i+=1
     i=0
@@ -435,11 +430,9 @@ def informacao2():
     print('[1] para alterar o nome do aluno: ')
     print('[2] para alterar matricula')
     print('[3] para alterar curso')
-    print('[4] para alterar presenças')
+    print('[4] para alterar telefone')
     print('[5] para alterar email')
-    print('[6] para alterar informações/observações')
-    print('[7] para alterar notas notas')
-    print('[8] Para deletar todos os dados de um aluno(a): ')
+    print('[6] Para deletar todos os dados de um aluno(a): ')
     print('[0] para voltar ao menu principal')
     print('---------------------------------------------')
 
@@ -475,7 +468,7 @@ while cadastrar:
             df = list(df)
 
             if escolher == '1':
-                print('\nTabela: Alunos - Matrícula:')
+                print('\nTabela: Alunos - Matrícula - Email:')
 
                 # Ler os dados do arquivo CSV
                 with open('registro de aluno fap 2024.csv', mode='r', encoding='utf8') as file:
@@ -483,7 +476,7 @@ while cadastrar:
                     rows = list(reader)
 
                 for row in rows:
-                    print('',row[0],'   -   ',row[1])
+                    print('',row[0],'   -   ',row[1], '  -  ',row[4])
 
             elif escolher == '2':
                 nome_pesquisa = input('Insira o nome do aluno(a): ')
@@ -511,36 +504,8 @@ while cadastrar:
         aluno_nome = input('Insira o nome: ').title()
         aluno_matricula = int(input('Insira o número de matricula: '))
         aluno_curso = input('Curso do aluno(a): ').title().rstrip()  #Tratar: adicionar opções
-
-        todas_as_notas = []
-
-        while True:
-            nota = float(input('\nInsira as notas do aluno(a): '))  #ordenar por unidades, se 2, 3 ou 4.
-            todas_as_notas.append(nota)
-            colocar_mais_notas = input('\nQuer colocar outra nota (s/n)? ').lower().rstrip()
-
-            if colocar_mais_notas == 's':
-                pass
-            elif colocar_mais_notas == 'n':
-                break
-            else:
-                print('\nPor favor, digite "n" para não registar outra nota e "s" para registrar.')
-
         aluno_telefone = input('Insira o número de telefone: ')   #tratar: verificar se nenhuma caractere str foi digitada
         aluno_email = input('Email do aluno(a): ')
-        aluno_presencas = int(input('presenças do(a) aluno (a): '))
-
-        while True:
-            aluno_informacao = input('Quer inserir alguma observação/informação sobre o aluno(a) (s/n)? ').lower().rstrip()
-
-            if aluno_informacao == 's':
-                info = input('Escrevas suas observações: ')
-                break
-            elif aluno_informacao == 'n':
-                info = 'Sem observações.'
-                break
-            else:
-                print('Por favor, digite "s" para adicionar uma informação ou "n" caso não queira.')
 
         aluno = {
                     'nome': aluno_nome,
@@ -548,17 +513,13 @@ while cadastrar:
                     'curso': aluno_curso,
                     'telefone': aluno_telefone,
                     'email':aluno_email,
-                    'presencas': aluno_presencas,
-                    'informacoes':info,
-                    'notas': todas_as_notas,
-                }
+            }
 
         print('\n\nTudo ok, aluno(a) cadastrado(a) com sucesso!')
 
-
-
         alunos.append(aluno) # adicionando o aluno na lista
-        salvar(alunos)
+        salvar(alunos) #salvando alunos na planilhas
+        alunos.remove(aluno) #retirando aluno da lista, para poder adicionar outro
 
 #---------------------------------------------------------------------
 #-----------------------#Continuar Cadastrando alunos-----------------
@@ -581,6 +542,8 @@ while cadastrar:
 
             #writer.writerow(('nome', 'matricula', 'curso','presenca(s)','email','informacoes','notas'))
             #def acessar(nome, matricula, mudanca,tipo, arquivo = 'registro de aluno fap 2024.csv'):
+
+            #alterar o nome
             if opcao == '1':
                 nome = input('\n Insira o nome a ser alterado ')
                 matricula = int(input('Insira o número de matrícula '))
@@ -588,6 +551,7 @@ while cadastrar:
 
                 acessar(nome = nome, mudanca = novo_nome, tipo = opcao, matricula = matricula)
 
+            #alterar o numero de matricula
             elif opcao == '2':
                 mudanca = int(input('\n Insira a matricula ser alterado'))
                 email_confirmar = input('Insira o email do aluno(a), por favor ')
@@ -596,63 +560,41 @@ while cadastrar:
 
                 acessar(mudanca = matricula_mudanca, tipo = opcao, email = email_confirmar)
 
-            elif opcao == '3':
 
+            #alterar o curso
+            elif opcao == '3':
                 nome = input('\nInsira o nome do aluno(a): ')
                 matricula = input('\n Insira o numero de matricula: ')
                 curso = input('Insira o novo curso: ')
 
-                acessar(mudanca = curso, tipo = opcao, nome = nome, matricula = matricula)#Corrigir cada interação
+                acessar(mudanca = curso, tipo = opcao, nome = nome, matricula = str(matricula))#Corrigir cada interação
 
+            #alterar o telefone
             elif opcao == '4':
-                nome = input('\n Insira o email a ser alterado')
+                nome = input('\n Insira o nome do(a) aluno(a): ')
                 matricula = input('Insira o numero de matricula: ')
-                presencas = input('Insira as presencas')
+                numero = input('Insira o novo número: ')
 
-                acessar(nome = nome, matricula = matricula, mudanca = presencas )
+                acessar(nome = nome, matricula = str(matricula), mudanca = numero )
 
+            #alterar email
             elif opcao == '5':
-                nome = input('\n Insira o email a ser alterado')
+                nome = input('\n Insira o nome do(a) aluno (a): ')
                 matricula = input('Insira o numero de matricula: ')
                 email = input('Insira o novo email: ')
-                acessar(nome = nome, matricula = matricula, mudanca = email)
+                acessar(nome = nome, matricula = str(matricula), mudanca = email, tipo = opcao)
 
             elif opcao == '6':
-                nome = input('\n Insira o nome do aluno(a): ')
-                matricula = input('Insira o numero de matricula: ')
-                info = input('Insira as informações/observações: ')
-                acessar(nome = nome, matricula = matricula, mudanca = info)
-
-            elif opcao == '7':
-                nome = input('\nInsira o nome do aluno(a): ')
-                matricula = input('\n Insira o numero de matricula: ')
-                novas_notas = []
-
-                while True:
-                    nota = float(input('\nInsira as notas do aluno(a): '))  #ordenar por unidades, se 2, 3 ou 4.
-                    novas_notas.append(nota)
-                    colocar_mais_notas = input('\nQuer colocar outra nota (s/n)? ').lower().rstrip()
-
-                    if colocar_mais_notas == 's':
-                        pass
-                    elif colocar_mais_notas == 'n':
-                        break
-                    else:
-                        print('\nPor favor, digite "n" para não registar outra nota e "s" para registrar.')
-
-                acessar(mudanca = novas_notas, nome = nome, matricula=matriculal )
-
-            elif opcao == '8':
                 nome = input('Insira o nome do(a) aluno(a) para apagar od dados: ')
                 matricula = input('Insira o numero de matricuila do(a) aluno(a): ')
 
-                acessar(mudanca = '', nome = nome, matricula = matricula, tipo = opcao)
+                acessar(mudanca = '', nome = nome, matricula = str(matricula), tipo = opcao)
 
             elif opcao == '0':  # sair do laço
                 break
 
             else:
-              print(f'Opção inválida. Você digitou {opcao}')
+              print(f'Opção inválida! Você digitou {opcao}')
 
     elif escolha == '0':
         cadastrar = False  # finaliza o programa.
