39c54d545482c801ee3462ce12787a0e322c600e
Victor Hugo Clmentino da Silva <151689447+Victor-HCSilva@users.noreply.github.com>
2024-07-05 14:35:34 -0300

    exercicio_13.py
    conclusão

--- exercicios_3_07_2024/exercicio_13.py
+++ exercicios_3_07_2024/exercicio_13.py
@@ -23,27 +23,45 @@ print(total)
 #result of chatgpt-16177.
 '''
 
-numeros = input('Operção: ')
+'''
+Implemente uma função que receba uma string contendo números e operações 
+matemáticas básicas (+, -, *, /) e retorne o resultado da expressão.
+- Concluido
+'''
+numeros = input('Operação: ')
 operador = [i for i in numeros if i in '*/+-']
+operador = ''.join(operador)
 n1 = []
 n2 = []
 i = 0
 
 while i < len(numeros):
     if numeros[i] in '+-*/':
+        print(numeros[i])
         #operador.append(numeros[i])
         break
-    n1.append(numeros[i])
-    i += 1
+    else:
+        n1.append(numeros[i])
+        i += 1
    
-
-#i += 1#para passar o index do operador na palavra
+i += 1#para passar o index do operador na string
 
 while i < len(numeros):
     n2.append(numeros[i])
     i+=1
 
-n1 = float(''.join(n1))
-n2 = float(''.join(n2))
+try:
+  n1 = float(''.join(n1))
+  n2 = float(''.join(n2))
+  if operador == '+':
+    print(n1 + n2)
+  elif operador == '-':
+    print(n1 - n2)
+  elif operador == '*':
+    print(n1 * n2)
+  elif operador == '/':
+    print(n1 / n2)
+except:
+  print('Deveconter uma operação básica')
 
-print(operador)
+
